bbfc6d731543ede0a020d7d5c32a924d7d386e80
edwin_ignas <edwinngugi38@gmail.com>
2023-12-02 00:11:39 +0300

    some refactoring

--- main.c
+++ main.c
@@ -5,6 +5,7 @@
 #include <stdbool.h>
 #include<signal.h>
 #include<ncurses.h>
+#include<setjmp.h>
 #include<ncursesw/ncurses.h>
 #include "voom.c"
 #include "param.h"
@@ -12,52 +13,63 @@
 //#define help "-help"
 WINDOW* mainwindow;
 void sig_handler(int sig);
+int editor();
 int startVoom(){
-    char ch;
-    int x,y,z,j,i,k,l,m,v,h;
-    z = j = i = v = 0;
-    h = 2;
-    char car[][85] = {"                  _..-------++._",
-    "              _.-'/ |      _||  \"--._",
-    "        __.--'`._/_\j_____/_||___\    `----.",
-    "    _.--'_____    |          \     _____    /",
-    " _j    /,---.\   |        =o |   /,---.\   |_             (( -o-))",
-    "[__]==// .-. \\==`===========/==// .-. \\=[__]        ( (  VOOOM!! ))",
-    ", - `._|\ `-' /|___\_________/___|\ `-' /|_.'  _____---(____--   --__==  )",
-    "        `---'                     `---'"};
-    printf("starting voom .....");
-    initscr();
-    getmaxyx(stdscr,y,x);
-    k = x - 1;
-    l = (x-1)/2;
-    m = l-37;
-    while(j != x){
-        move(z,j);
-        printw("*");
-        j++;
+    int x,y,a,b,c,d,e,f;
+    c = 0; 
+    char voom[][50] = {"_ _    _ _ _ __    _________      ___","\\  \\  /  //    \\  /    \\ |  \\    /   |"," \\  \\/  //  __  \\/  __  \\|   \\></    |","  \\    / \\ (__) /\\ (__) /|  *\\   *\\  |","   \\__/   \\_ __/  \\____/ |_/  \\_/  \\_|"} ;
+    y = LINES;
+    x = COLS;
+    a = (x / 2) - 20;
+    b = (y / 2);
+    d = 2;
+    attron(A_BOLD | COLOR_PAIR(2));
+    while(c != 5){
+        move(d,a);
+        printw("%s",voom[c]);
+        c++;
+        d++;
     }
-    while(z != 10){
-        move(z,i);
-        printw("*");
-        move(z,k);
-        printw("*");
-        z++;
-    }
-
-    while(v != 8){
-        move(h,m);
-        printw("%s",car[v]);
-        h++;
-        v++;
-    }
-     refresh();
+    attroff(A_BOLD | COLOR_PAIR(2));
+    attron(COLOR_PAIR(3));
+    printw("..Editor");
+    attroff(COLOR_PAIR(3));
+    d++;
+    e = (x / 2)-6;
+    move(d,e);
+    printw("VOOM v0.0.1\n");
+    d++;
+    f = (x / 2)-27;
+    move(d,f);
+    printw("voom is a simple open source vim prototypic text editor.\n\n");
+    d += 2;
+    e = (x / 2) -13;
+    move(d,e);
+    attron(A_BOLD | COLOR_PAIR(2));
+    printw("#)Author  :");
+    attroff(A_BOLD | COLOR_PAIR(2));
+    attron(COLOR_PAIR(4));
+    printw(" Edwin_ignas   \n");
+    attroff(COLOR_PAIR(4));
+    d++;
+    move(d,e);
+    attron(A_BOLD | COLOR_PAIR(2));
+    printw("#)Website :");
+    attroff(A_BOLD | COLOR_PAIR(2));
+    attron(COLOR_PAIR(4));
+    printw(" Edwinignas.com\n");
+    attroff(COLOR_PAIR(4));
+    editor();
+    move(0,3);
+    printw("1");
+    refresh();
     getch();
-    endwin();
     return EXIT_SUCCESS;
 }
+
 int voomHelp(){
-    printf("starting voom................\n\n");
-    printf("+++++++++++++++++++++++++++++++++++++++++++++++++++++++=\n");
+    printf("starting voom....\n\n");
+    printf("+++++++++++++++++++++++++++++++++++++++++++++++++++++++++\n");
     printf("                     VOOM EDITOR                         \n");
     printf("                                     version 0.1         \n");
     printf("+++++++++++++++++++++++++++++++++++++++++++++++++++++++++\n");
@@ -68,7 +80,6 @@ int voomHelp(){
 }
 int renderer(char* argv[]){
     FILE* fptr;
-    printw("function called");
     char char_sz;
     char count = 0;
     int n = 10000;
@@ -77,8 +88,6 @@ int renderer(char* argv[]){
     char* buffer = (char*)malloc(n * sizeof(char));
     char* buffer3 = buffer;
     char** buffer2 = &buffer;
-    printw("%ld",sizeof(count));
-    printw("%ld",sizeof(buffer));
     while(ch != EOF){
         ch = fgetc(fptr);
         if(ch == EOF)break;
@@ -87,21 +96,16 @@ int renderer(char* argv[]){
         count++;
         //*buffer2++;
     }
-int v,h;
-v = buffer;
-h = buffer3;
-printw("were here now");
-printw("%d\n",v);
-printw("%d",h);
 int y = 0;
 int x = 5;
+fclose(fptr);
 
 while(buffer != buffer3){
     cha = *buffer;
     move(y,x);
     printw("%c",cha);
     x++;
-    if(x == (winsz.ws_col - 1)){
+    if(x == (COLS - 1)){
         y++;
         x = 5;
     }
@@ -111,11 +115,29 @@ refresh();
 return EXIT_SUCCESS;
 }
 
+// int screenPrint_proc(){
+//     if(ch == "\n"){
+//         moveCursor(n);
+//     }else if(ch == 08){
+//         movecursor(-1);
+//     }else if(ch = 27){
+//         moveCursor(escape);
+//     }else if(ch == 09){
+//         moveCursor(tab);
+//     }
+
+// }
+
 int workplace(){
+    
+
+
+
+
     int dot_count,line_count,ed_lx,y,x;
     line_count = dot_count = 0;
-    y = winsz.ws_row;
-    x = winsz.ws_col;
+    y = LINES;
+    x = COLS;
     ed_l = y - 2;
     int ed_lx1,ed_lx2;
     ed_lx = 3;
@@ -141,9 +163,7 @@ int workplace(){
 refresh();
 return EXIT_SUCCESS;
 }
-
-WINDOW* editor(mainwindow)
-WINDOW* mainwindow;
+int editor()
 {
     int x,y,r,t;
     r = 200;
@@ -151,103 +171,128 @@ WINDOW* mainwindow;
     char ch;
     char *cmd= "Voom=> ";
     noecho();
-    if(has_colors()){
-        start_color();
-        init_color(COLOR_RED,17,64,86);
-        init_pair(1, COLOR_WHITE, COLOR_RED);
-        //use_default_colors();
-        attron(1);
-        wbkgd(mainwindow, COLOR_PAIR(1));
-        wrefresh(mainwindow);
-    }
-    /*while(ch != 10){
-        wbkgd(mainwindow, COLOR_PAIR(1));
-        //wattron(mainwindow,COLOR_PAIR(1));
-         wrefresh(mainwindow);
-        ch = getch();
-        printw("%c",ch);
-        refresh();
-    }*/
-    cmd_p = (winsz.ws_row- 1);
-    vm_sy = (winsz.ws_row - 2);
+    y = LINES;
+    x = COLS;
+    cmd_p = (y - 1);
+    vm_sy = (y - 2);
     vm_sx = 1;
-    ln_ny = (winsz.ws_row - 2);
-    ln_nx = (winsz.ws_col - 20);
+    ln_ny = (y - 2);
+    ln_nx = (x - 20);
+     
     move(cmd_p,0);
-    printw("%s",cmd);
+    addstr(cmd);
     move(vm_sy,vm_sx);
     printw("normal");
     move(ln_ny,ln_nx);
     printw("line %d, column %d",r,t);
-    refresh();
-    workplace();
-    //getch();
-    //renderer(); 
-return mainwindow;
-}
-int setupwin_param(WINDOW* mainwindow){    
-getmaxyx(mainwindow,winsz.ws_row,winsz.ws_col);
-//winsize *winsz2 = &winsz;
+    refresh(); 
 return EXIT_SUCCESS;
-}
-WINDOW* setupwin(){
-    WINDOW* mainwindow;
-    if (initscr() == NULL){
-        fprintf(stderr, "Could not initialize ncurses!\n");
-        exit(EXIT_FAILURE);
-    }
-    mainwindow = stdscr;
-return mainwindow;
 }
 
 // sets up voom window and the diffrent parameters required;
 
-WINDOW* voomWindow_setup(char* argv[]){
+int voomWindow_setup(){
     if(setup_f == true){
         delwin(mainwindow);
         setup_f = false;
     }
-    WINDOW* mainwindow;
-    mainwindow = setupwin();
-    printf("reached here\n");
-    setupwin_param(mainwindow);
-    mainwindow = editor(mainwindow);
-    renderer(argv);
-    getch();
-    return mainwindow;
+    wrefresh(stdscr);
+    getmaxyx(stdscr,LINES,COLS);
+    editor();
+    workplace();
+    return EXIT_SUCCESS;
+}
+int setup_screen(){
+    if (initscr() == NULL){
+        fprintf(stderr, "Could not initialize ncurses!\n");
+        exit(EXIT_FAILURE);
+}
+    if(has_colors()){
+        start_color();
+        init_color(COLOR_RED,17,64,86);
+        init_color(COLOR_GREEN,12, 237, 19);
+        init_color(COLOR_BLUE,237, 12, 203);
+        init_color(COLOR_YELLOW,237, 237, 12);
+        init_pair(1, COLOR_WHITE, COLOR_RED);
+        init_pair(2,COLOR_GREEN,COLOR_RED);
+        init_pair(3,COLOR_BLUE,COLOR_RED);
+        init_pair(4,COLOR_YELLOW,COLOR_RED);
+        //use_default_colors();
+        attron(1);
+        wbkgd(stdscr, COLOR_PAIR(1));
+        wrefresh(stdscr);
+    }
+clear();
+return EXIT_SUCCESS;
 }
 void sig_handler(int sig)
 {
   if (SIGWINCH == sig) {
-    endwin();
     setup_f = true;
-    static WINDOW* mainwindow2;
-    printf("terminal window resized\n");
-    signal(SIGWINCH,sig_handler);
-    mainwindow2 = voomWindow_setup(argv);
-    //editor(mainwindow,winsz);
-
+    if(setup_f == true){
+        endwin();
+        setup_f = false;
+    }
+    endwin();
+    setup_screen();
+    voomWindow_setup();
+    renderer(filep);
 }
 }
 int main(int argc,char **argv){
     char *help = "-help";
     char *help2 = "-h";
     char* v = argv[1];
+    int x = 0;
+    filep = argv;
     //FILE* edit = fopen(argv[1],"w+");
     signal(SIGWINCH,sig_handler);
-    if(argc == 1){
-        startVoom();
+    
+    if((argc > 1) && ((strcmp(v,help) == 0)||(strcmp(v,help2) == 0))){
+        voomHelp();
+    }else{
+        setup_screen();
+        if(argc == 1){
+        while(x != 3){
+            startVoom();
+            delay(7000);
+            printw("%d curse init",x);
+            x++;
+        }
+        goto end;
+        }
+    if(argc > 1){
+        voomWindow_setup();
+        renderer(filep);
+        delay(3000);
+    }
+    }
+ /*if(argc == 1){
+        initscr();
+        while(x != 3){
+            startVoom();
+            delay(700);
+            printw("%d curse init",x);
+            x++;
+        }
+        endwin();
+
     }else if(argc > 1){
        if((strcmp(v,help)) == 0||(strcmp(v,help2)) == 0){
         voomHelp();
        }else{
-        mainwindow = voomWindow_setup(argv);
+        filep = argv;
+        voomWindow_setup(filep);
         //renderer(argv);
         //getch();
 
        }
     }
-delwin(mainwindow);
+while(getch() != 10){
+    voomWindow_setup(filep);
+    delay(700);
+}*/
+end:    
 endwin();
 return 0;
 }
--- param.h
+++ param.h
@@ -16,11 +16,11 @@ EXTERN int ln_nx INIT(= 0);
 EXTERN int cl_ny INIT(= 0);
 EXTERN int cl_nx INIT(= 0);
 EXTERN int ed_l  INIT(= 0);
-EXTERN int setup_f INIT(= 0);
+EXTERN int setup_f INIT(= false);
 //windows parameters
 typedef struct winsize
 {
       unsigned short ws_row;
       unsigned short ws_col;
 }winsize;
-winsize winsz;
+char** filep;
